c82c7ab731474f8e8acd72afdee1d3c36fc65329
Maureen435 <mchepkemoi435@gmail.com>
2020-06-25 16:46:57 +0300

    added a class

---
+++ password.py
@@ -0,0 +1,11 @@
+class User
+      user_list = []
+      def _init_(self,user_name,password):
+          self.user_name = user_name
+          self.password = password
+      def save_user(self):
+          User.user_list.append(self)
+      def delete_user(self)
+          User.user_list.remove(self)
+
+    
