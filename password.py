c9f050f4f5f7a107a38624e7789a515cea894fe6
Maureen435 <mchepkemoi435@gmail.com>
2020-06-25 16:58:20 +0300

    added a class

--- password.py
+++ password.py
@@ -1,11 +1,20 @@
 class User
       user_list = []
-      def _init_(self,user_name,password):
+      def _init_(self,user_name,user_password):
           self.user_name = user_name
-          self.password = password
+          self.user_password = user_password
       def save_user(self):
           User.user_list.append(self)
-      def delete_user(self)
+      def delete_user(self):
           User.user_list.remove(self)
 
-    
+class Credential    
+      credential_list = []
+      def _init_(self,account,account_username,account_password):
+          self.account = account
+          self.account_username = account_username
+          self.account_password = account_password
+      def save_credential(self):
+          Credential.credential_list.append(self)
+      def delete_credential(self):
+          Credential.credential_list.remove(self)
